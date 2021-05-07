ab1bfa715f6b5de2b0818e1a13b26abbce88f386
Glynn Lane <lanegs@uwm.edu>
2021-05-07 11:31:55 -0500

    More cleanup

--- Hudl/HudlFileManager.py
+++ Hudl/HudlFileManager.py
@@ -15,9 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Some code borrowed from pyasar:
-# https://github.com/Photonios/pyasar
 """See docstring for HudlFileManager class"""
 
 import glob
